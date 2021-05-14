167a5417c78fb8783211b1da831b0c3d53970fc5
Alex Roth <alex@magmastone.net>
2021-05-14 15:34:24 -0400

    figure out networking interface

--- pkg/networking/networking.go
+++ pkg/networking/networking.go
@@ -7,6 +7,8 @@
 // Internet routing/NAT may be useful to you as well...
 package networking
 
+import "net"
+
 // NetworkManager represents something that can handle creating an isolated network for VMs to live on.
 // Managers are initialized, and then asked to create tap devices.
 // The netns name can be retrieved to allow processes that need direct access to the netns to jump in there.
@@ -14,4 +16,26 @@ package networking
 // All VMs will be assigned IPs in that range.
 // (sorry in advance if the type name triggers flashbacks. I promise this NetworkManager actually does what you want it to do.)
 type NetworkManager interface {
+	GetVMNetns() string
+	ReleaseTap(ifce TAPInterface) error
+	CreateTap() (TAPInterface, error)
+}
+
+// InitializeNetworkManager creates a NetworkManager
+type InitializeNetworkManager func(vmSubnet string) (NetworkManager, error)
+
+// CreateBridgingNetworkManager will initialize a NetworkManager
+// which creates a bridge in a secondary network namespace.
+// A small /31 is carved out at the top of vmSubnet to provide connectivity to/from the main subnet.
+func CreateBridgingNetworkManager(vmSubnet string) (NetworkManager, error) {
+
+}
+
+// TAPInterface describes a TAP device, as well as it's MAC & IP assignment
+type TAPInterface interface {
+	Name() string
+	Idx() int
+	MAC() string
+	IP() net.IP
+	Netmask() net.IPMask
 }
